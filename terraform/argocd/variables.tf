bf39cf04d548711655d9d47896de452ef2e60387
Raman Hrabun <rhdocld@gmail.com>
2023-10-25 12:41:43 +0200

    Refactor variables

--- terraform/argocd/variables.tf
+++ terraform/argocd/variables.tf
@@ -1,25 +1,36 @@
 # Cluster
 variable "k8s_context" {
   description = "The name of the k8s context to use"
+  type        = string
   default     = "docker-desktop"
 }
 
 # ArgoCD
 variable "argocd_repo" {
-  default = "https://argoproj.github.io/argo-helm"
+  description = "The URL of the ArgoCD Helm Chart repository"
+  type        = string
+  default     = "https://argoproj.github.io/argo-helm"
 }
 variable "argocd_chart_name" {
-  default = "argo-cd"
+  description = "The name of the ArgoCD Helm Chart"
+  type        = string
+  default     = "argo-cd"
 }
 
 variable "chart_version" {
-  default = "5.46.8"
+  description = "The version of the ArgoCD Helm Chart"
+  type        = string
+  default     = "5.46.8"
 }
 
 variable "argocd_namespace" {
-  default = "argocd"
+  description = "The name of the namespace for ArgoCD to be deployed in"
+  type        = string
+  default     = "argocd"
 }
 
 variable "argocd_release_name" {
-  default = "argocd"
+  description = "The name of the ArgoCD release"
+  type        = string
+  default     = "argocd"
 }
